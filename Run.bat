badec55a967c29b2f717967517d4c223d6a4b69c
nicegamer7 <kerminaawad@gmail.com>
2018-04-26 13:28:45 -0400

    Add files via upload

---
+++ Run.bat
@@ -0,0 +1,37 @@
+@echo off
+cls
+
+echo Fonter - ng7
+
+setlocal enabledelayedexpansion
+set "_drvs=Z: Y: X: W: V: U: T: S: R: Q: P: O: N: M: L: K: J: I: H: G: F: E: D: "
+
+for /f "skip=1" %%u in ('wmic logicaldisk get deviceid') do set "_drvs=!_drvs:%%u =!"
+
+set _drv=%_drvs:~0,2%
+set _idir=%~dp0
+
+if %_idir:~-1%==\ (set _bdir=%_idir:~0,-1%) else (set _bdir=%_idir%)
+
+if %_bdir:~0,2%==\\ goto nas
+
+:local
+set condition=
+
+
+set _fdir=%_bdir%
+goto process
+
+:nas
+set condition=nas
+
+net use %_drv% "%_bdir%"
+set _fdir=%_drv%
+z:
+
+:process
+for /r %%f in (*.ttf *.ttc *.otf) do %_fdir%\RegisterFont.exe add "%%f"
+
+:exit
+echo Well that was fun, we're done now, thanks.
+pause
